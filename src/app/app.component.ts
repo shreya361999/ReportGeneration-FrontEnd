acca4c15dc909ec0e3c0db68a4dead57a307dfbd
Shreya Kalal <Shreya.Kalal@BRILLIO.COM>
2023-05-26 16:33:08 +0530

    Excel Full Report Generated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { saveAs } from 'file-saver';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'report-generation';
-}
+  constructor(private http: HttpClient) {}
+
+  downloadReport() {
+    const url = 'http://localhost:8080/projects/fullReportExport';
+    this.http.get(url, { responseType: 'blob' }).subscribe((response: Blob) => {
+      const fileName = 'projectsFullReport.xlsx';
+      saveAs(response, fileName);
+    });
+  }
+  
+  
+  
+
+  // downloadReport(): void {
+  //   const headers = new HttpHeaders({
+  //     'Content-Type': 'application/json',
+  //   });
+
+  //   this.http
+  //     .get('http://localhost:8080/projects/fullReportExport', {
+  //       headers,
+  //       responseType: 'blob',
+  //     })
+  //     .subscribe((response: Blob) => {
+  //       const filename = 'projectsFullReport.xlsx';
+  //       saveAs(response, filename);
+  //     });
+  // }
+  
+  }
+
